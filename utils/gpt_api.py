8fb4e0501d4915e5f8f75d8125f8e6f55f266ae4
rootuser <kyre0@bk.ru>
2024-11-21 16:45:46 +0400

    integrated gpt-4o-mini

--- utils/gpt_api.py
+++ utils/gpt_api.py
@@ -1,22 +1,42 @@
+import aiohttp
 from openai import OpenAI
 
 from src.config import BotConfig
+from utils.prompts import generate_text_prompt, generate_photo_prompt
 
-client = OpenAI(api_key=BotConfig.gpt_token, )
+
+async def gpt_api_func_text(prompt, style):
+    async with aiohttp.ClientSession() as session:
+        async with session.post(
+                "https://api.openai.com/v1/chat/completions",
+                headers={"Authorization": f"Bearer {BotConfig.gpt_token}"},
+                json={
+                    "model": "gpt-4o-mini",
+                    "messages": [
+                        {"role": "user", "content": f'{generate_text_prompt.replace("STYLE", style)}: {prompt}'},
+                    ],
+                    "temperature": 0
+                }
+        ) as response:
+            print(await response.text())
+            response = await response.json()
+            return response['choices'][0]['message']['content'], response['usage']['total_tokens']
 
 
-async def gpt_api_func(prompt, style):
-    response = client.chat.completions.create(
-        model="gpt-4o",
-        messages=[{"role": "system"},
-                  {
-                      "role": "user",
-                      "content": [
-                          {"type": "text", "text": prompt.replace('STYLE', style)},
+async def gpt_api_func_prompt(prompt, style):
+    async with aiohttp.ClientSession() as session:
+        async with session.post(
+                "https://api.openai.com/v1/chat/completions",
+                headers={"Authorization": f"Bearer {BotConfig.gpt_token}"},
+                json={
+                    "model": "gpt-4o-mini",
+                    "messages": [
+                        {"role": "user", "content": f'{generate_photo_prompt.replace("STYLE", style)}: {prompt}'},
+                    ],
+                    "temperature": 0
+                }
+        ) as response:
+            print(await response.text())
+            response = await response.json()
+            return response['choices'][0]['message']['content'], response['usage']['total_tokens']
 
-                      ],
-                  },
-                  ],
-        temperature=0
-    )
-    return response
